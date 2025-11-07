dfe8e8180c621264d8a5eab35fa4616ea172c105
Vladimir Kozelkov <46866705+vova7878@users.noreply.github.com>
2025-11-07 23:41:17 +0700

    Set up publications to Maven Central

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,8 @@ android {
     }
 
     publishing {
-        singleVariant("release") {
+        multipleVariants {
+            allVariants()
             withSourcesJar()
         }
     }
@@ -26,16 +27,43 @@ android {
 dependencies {
 }
 
-publishing {
-    publications {
-        register<MavenPublication>("release") {
-            groupId = project.group.toString()
-            artifactId = project.name
-            version = project.version.toString()
 
-            afterEvaluate {
-                from(components["release"])
+mavenPublishing {
+    publishToMavenCentral(automaticRelease = false)
+    signAllPublications()
+
+    coordinates(
+        groupId = "io.github.vova7878",
+        artifactId = "SunCleanerStub",
+        version = project.version.toString()
+    )
+
+    pom {
+        name.set("SunCleanerStub")
+        description.set("Wrapper over sun.misc.Cleaner for Android")
+        inceptionYear.set("2025")
+        url.set("https://github.com/vova7878/SunCleanerStub")
+
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://opensource.org/license/mit")
+                distribution.set("repository")
             }
+        }
+
+        developers {
+            developer {
+                id.set("vova7878")
+                name.set("Vladimir Kozelkov")
+                url.set("https://github.com/vova7878")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/vova7878/SunCleanerStub")
+            connection.set("scm:git:git://github.com/vova7878/SunCleanerStub.git")
+            developerConnection.set("scm:git:ssh://git@github.com/vova7878/SunCleanerStub.git")
         }
     }
 }
