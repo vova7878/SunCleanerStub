dfce082c0f235cdcaec401d4b93a9f6c47c74788
Vladimir Kozelkov <46866705+vova7878@users.noreply.github.com>
2026-03-01 18:39:22 +0700

    Update gradle files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,6 @@
 import com.vanniktech.maven.publish.AndroidMultiVariantLibrary
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.SourcesJar
 
 plugins {
     alias(libs.plugins.android.library)
@@ -11,8 +13,12 @@ java {
 }
 
 android {
+    enableKotlin = false
+
     namespace = "com.v7878.sun.cleaner"
-    compileSdk = 36
+    compileSdk {
+        version = release(36)
+    }
 
     defaultConfig {
         minSdk = 26
@@ -27,8 +33,8 @@ mavenPublishing {
     signAllPublications()
     configure(
         AndroidMultiVariantLibrary(
-            sourcesJar = true,
-            publishJavadocJar = false,
+            javadocJar = JavadocJar.Empty(),
+            sourcesJar = SourcesJar.Sources()
         )
     )
 
